54e9f7c46c9430ad800b53eb366efce442f285a8
MarcMas29 <a2208018@institutmontilivi.cat>
2023-01-23 17:31:47 +0100

    menu

---
+++ Activity3/Activity3/Program.cs
@@ -0,0 +1,66 @@
+﻿internal class Program
+{
+    private static void Main(string[] args)
+    {
+        //prova de push
+        ConsoleKeyInfo tecla;
+        do
+        {
+            Console.Clear();
+            ShowOptions();
+            tecla = Console.ReadKey();
+            Console.Clear();
+            switch (tecla.Key)
+            {
+                case ConsoleKey.D1:
+                    Geography();
+                    break;
+
+               /* case ConsoleKey.D2:
+                    History();
+                    break;
+
+                case ConsoleKey.D3:
+                    Science();
+                    break;*/
+
+                case ConsoleKey.D0:
+                    MsgNextScreen("Press any key to exit");
+                    break;
+
+                default:
+                    MsgNextScreen("Error. Prem una tecla per a tornar al menu... ");
+                    break;
+
+            }
+        } while (tecla.Key != ConsoleKey.D0);
+    }
+    public static void ShowOptions()
+    {
+        Console.WriteLine("1- Geography");
+        Console.WriteLine("2- History");
+        Console.WriteLine("3- Science");
+    }
+    public static void MsgNextScreen(string msg)
+    {
+        Console.WriteLine(msg);
+        Console.ReadKey();
+    }
+    public static void Geography()
+    {
+        char a, b, c, d;
+        Console.WriteLine("");
+        try
+        {
+            
+        }
+        catch (Exception e)
+        {
+            Console.WriteLine(e.Message);
+        }
+        finally
+        {
+            MsgNextScreen("PRESS A KEY TO GO TO THE MAIN MENU");
+        }
+    }
+}
